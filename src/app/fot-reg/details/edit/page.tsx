ff3afd6a06fdf16254f77371812b975127e4cf81
Patricia Palacios <patri_postigo@hotmail.com>
2026-04-13 17:14:05 +0200

    Hide horizontal overflow on FoT registration edit mobile

--- src/app/fot-reg/details/edit/page.tsx
+++ src/app/fot-reg/details/edit/page.tsx
@@ -202,7 +202,7 @@ export default function FotRegEditPage() {
   const formDisabled = saving || saved;
 
   return (
-    <main className={`${forms.page} ${forms.pageNarrow}`}>
+    <main className={`${forms.page} ${forms.pageNarrow}`} style={{ overflowX: "hidden" }}>
       <h1 className={forms.h1}>{headingText}</h1>
       <div className={forms.backRow}>
         <BackLink fallbackHref="/fot-reg" className={`${forms.linkButton} ${forms.linkButtonLight}`}>
